8b8a5b126fda22e814b2528b7ff65e4e9f12628c
patel-malav <patelmalav64@gmail.com>
2020-01-18 01:44:48 +0530

    refresh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ExploreComponent } from './explore/explore.component';
-import { AccountComponent } from './account/account.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/explore',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: AccountComponent
-  },
-  {
-    path: 'account',
-    component: AccountComponent
-  },
-  {
-    path: 'register',
-    component: AccountComponent
-  },
-  {
-    path: 'explore',
-    component: ExploreComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ExploreComponent } from './explore/explore.component';
 
 // Angular Material Modules
 import {MatMenuModule} from '@angular/material/menu';
@@ -13,7 +11,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
-import { AccountComponent } from './account/account.component'; 
 import {MatInputModule} from '@angular/material/input'; 
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
@@ -23,10 +20,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    ExploreComponent,
-    AccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
